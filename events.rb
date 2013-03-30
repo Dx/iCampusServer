24275c7fb7b3b6fad10192cbd892d72b53a2abe5
David Cruz <dequis@gmail.com>
2013-03-30 17:01:13 -0600

    Probando acceso a cloudant 2

--- events.rb
+++ events.rb
@@ -2,24 +2,24 @@ require 'sinatra'
 require 'rest_client'
 require 'json'
 
+DB = "#{ENV['https://app13659298.heroku:55Vha7nOQVFFOdgAQILo53qO@app13659298.heroku.cloudant.com']}/events"
+
 get '/' do
   'Hello, world'
 end
-
-get '/events/' do  
-	DB = "#{ENV['https://app13659298.heroku:55Vha7nOQVFFOdgAQILo53qO@app13659298.heroku.cloudant.com']}/events"
-	get "/doc/:doc" do
+	
+get "/events/:doc" do
 	doc = RestClient.get("#{DB}/#{params[:doc]}")
 	@result = JSON.parse(doc)
 	haml :doc_id
+end
 
-
-
+# get '/events/' do 
   # "{'events':
   #     [
   #       {'initialdate':'2013-07-30', 'finaldate':'2013-08-04', 'name':'Campus Party 2013 MX', 'place':'Expo Santa Fe', 'city':'Mexico DF'}
   #       {'initialdate':'2013-06-01', 'finaldate':'2013-06-17', 'name':'Cumple', 'place':'Casa', 'city':'Mexico DF'}
   #     ]
   # }"
-end
+# end
 
