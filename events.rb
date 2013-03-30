df272e40e703752600ee9a05138230520953e997
David Cruz <dequis@gmail.com>
2013-03-30 16:57:11 -0600

    Probando acceso a cloudant

--- events.rb
+++ events.rb
@@ -7,11 +7,19 @@ get '/' do
 end
 
 get '/events/' do  
-  "{'events':
-      [
-        {'initialdate':'2013-07-30', 'finaldate':'2013-08-04', 'name':'Campus Party 2013 MX', 'place':'Expo Santa Fe', 'city'='Mexico DF'}
-        {'initialdate':'2013-06-01', 'finaldate':'2013-06-17', 'name':'Cumple', 'place':'Casa', 'city'='Mexico DF'}
-      ]
-  }"
+	DB = "#{ENV['https://app13659298.heroku:55Vha7nOQVFFOdgAQILo53qO@app13659298.heroku.cloudant.com']}/events"
+	get "/doc/:doc" do
+	doc = RestClient.get("#{DB}/#{params[:doc]}")
+	@result = JSON.parse(doc)
+	haml :doc_id
+
+
+
+  # "{'events':
+  #     [
+  #       {'initialdate':'2013-07-30', 'finaldate':'2013-08-04', 'name':'Campus Party 2013 MX', 'place':'Expo Santa Fe', 'city':'Mexico DF'}
+  #       {'initialdate':'2013-06-01', 'finaldate':'2013-06-17', 'name':'Cumple', 'place':'Casa', 'city':'Mexico DF'}
+  #     ]
+  # }"
 end
 
